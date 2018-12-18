8deba2bbfdb265e3a6afc4883a045349aec5371e
Eva Dahloe <daheva15@student.westerdals.no>
2018-12-18 11:44:39 +0100

    Added metrics

--- src/main/java/com/example/herokupipeexample/CustomerController.java
+++ src/main/java/com/example/herokupipeexample/CustomerController.java
@@ -3,8 +3,10 @@ package com.example.herokupipeexample;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
+import com.codahale.metrics.MetricRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CustomerController {
 
+    @Autowired
+    private MetricRegistry registry;
+
     private CustomerRepository customerRepository;
 
-    public CustomerController(CustomerRepository customerRepository) {
+    public CustomerController(CustomerRepository customerRepository, MetricRegistry registry) {
       this.customerRepository = customerRepository;
+      this.registry = registry;
     }
 
     @RequestMapping("/")
@@ -26,6 +32,7 @@ public class CustomerController {
 
         logger.info("Testing logz.io!");
         logger.warn("Winter is coming");
+        registry.meter("welcome").mark();
         return "Welcome to this small REST service. It will accept a GET on /list with a request parameter lastName, and a POST to / with a JSON payload with firstName and lastName as values.";
     }
 
